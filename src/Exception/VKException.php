1a4c034bcaeaedf9e75157d7f782a94b173248ae
banel1ng <ohot2015@gmail.com>
2021-08-16 17:35:07 +0300

    add vk

---
+++ src/Exception/VKException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * The exception class for VK library.
+ * @author Vlad Pronsky <vladkens@yandex.ru>
+ * @license https://raw.github.com/vladkens/VK/master/LICENSE MIT
+ */
+
+namespace App\Exception;
+ 
+class VKException extends \Exception {  }
+
