86351739445dffa1b6da5ba0c4b94185c5729eec
banel1ng <ohot2015@gmail.com>
2022-02-07 17:44:31 +0300

    добавил ответ пользователю

--- src/Controller/HookVkController.php
+++ src/Controller/HookVkController.php
@@ -26,13 +26,13 @@ class HookVkController extends AbstractController
             dump('no data');
             return new Response('ok');
         }
+        dump($data);
         switch ($data['type']) {
             case 'confirmation':
                 return new Response('d26cdef1');
             case 'message_new':
                 $message = $data['object']['message'];
-                dump($message['text']);
-                if (empty($message['text'])){
+                if (empty($message['text']) || $message['peer_id'] === 2000000001){
                     dump('break');
                     break;
                 }
@@ -41,18 +41,38 @@ class HookVkController extends AbstractController
                     dump('filtert spam detected');
                     return new Response('ok');
                 }
-                dump( $message['text'] .'  '. PHP_EOL . PHP_EOL . 'от пользователя: '. PHP_EOL . '@id'.$message['from_id']);
-                dump($this->getParameter('app_token'));
-                $rsPost = $vk->api('wall.post', [
+                $user = $vk->api('users.get', [
+                    'user_ids' => $message['from_id'],
+                    'access_token' => $this->getParameter('app_token'),
+                ], 'array', 'POST');
+
+
+                $user = $user['response'][0];
+                $vk->api('messages.send', [
+                    'message' => $user['first_name'] . " Я размещу твое сообщение на стене группы после того как оно пройдет модерацию. Обычно модерация не занимает более получаса.",
+                    'peer_id' => $message['from_id'],
+                    'access_token' => $this->getParameter('group_access_token'),
+                    'v' => '5.103',
+                    'random_id' => time()
+                ], 'array', 'POST');
+
+                $vk->api('wall.post', [
                     'owner_id' => $VK_GROUP_MY,
                     'from_group' => 1,
-                    'message'=> $message['text'] .'  '. PHP_EOL . PHP_EOL . 'от пользователя: '. PHP_EOL . '@id'.$message['from_id'],
-                    //'publish_date' => time() + (60 * 30 ),
+                    'message'=> sprintf('%s %s%sот пользователя: %s[id%s|%s %s]',
+                        $message['text'],
+                        PHP_EOL,
+                        PHP_EOL,
+                        PHP_EOL,
+                        $message['from_id'],
+                        $user['first_name'],
+                        $user['last_name']
+                    ),
+                    'publish_date' => time() + (60 * rand(3, 30)),
                     'access_token' => $this->getParameter('app_token'),
                     'count' => 10,
                 ], 'array', 'POST');
 
-                dump($rsPost);
                 break;
         }
 
