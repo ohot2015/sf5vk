e6f91c9ac12b1eb885aab4472ef6823a83fb24d7
ohot2015@gmail.com <B41b5ieq!>
2022-06-02 16:24:18 +0300

    fix restart container

--- src/Controller/HookVkController.php
+++ src/Controller/HookVkController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 use App\Service\SpamFilter;
 use App\Service\WallService;
+use Google\Cloud\Dialogflow\V2\EntityTypesClient;
+use Google\Cloud\Dialogflow\V2\IntentsClient;
+use Google\Cloud\Dialogflow\V2\QueryInput;
+use Google\Cloud\Dialogflow\V2\SessionsClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HookVkController extends AbstractController
 {
-
     /**
      * @Route("/", name="hook_vk")
      */
@@ -18,6 +22,7 @@ class HookVkController extends AbstractController
     {
       //  return new Response('ok');
         $data = json_decode($request->getContent(), true);
+        putenv('GOOGLE_APPLICATION_CREDENTIALS='.$this->getParameter('kernel.project_dir').'/service-account.json');
 
         if (empty($data['type'])) {
             dump('no data');
@@ -38,8 +43,19 @@ class HookVkController extends AbstractController
 //                    return new Response('ok');
 //                }
 
+                //$projectId = 'vk-bot-350515';
+                //$entityTypeId = $message['from_id'];
+                $sessionsClient = new SessionsClient();
+                try {
+                    $session = $message['from_id'];
+                    $queryInput = new QueryInput();
+                    $response = $sessionsClient->detectIntent($session, $queryInput);
+                    dump($response);
+                } finally {
+                    $sessionsClient->close();
+                }
                 $mes = 'test';
-                //,
+
                 //$wall->postMessageInWall($message['from_id'],$message['text']);
                 $wall->himSelfSend('test');
 
